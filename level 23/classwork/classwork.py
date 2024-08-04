30825d2feaa85e33ecf5602ac0c8ef66ed026f34
gikosin8 <giorgilekiashvili860@gmail.com>
2024-08-04 10:08:54 -0700

    commit

---
+++ level 23/classwork/classwork.py
@@ -0,0 +1,20 @@
+#age = 20
+#if age > 18:
+#    print('you can take license')
+#elif 30 > 18:
+#    print("u can take")
+#else:
+#   print('you are younger and you cannot take a license')
+
+name = input('Enter ur name: ')
+
+if name == 'giorgi':
+    print(  "Hello " '' + name)
+else:
+    print("it's wrong name, so goodbye")
+
+
+
+
+
+ 
---
+++ level 23/homework/homework2.py
@@ -0,0 +1,9 @@
+A = input("Hello, do you want to get a driver's license?: ")
+age = 20
+
+B = int(input("Enter your age to get a driver's license: "))
+if B > age:  
+    print('you can take license')
+
+else:
+   print('you are younger and you cannot take a license')
