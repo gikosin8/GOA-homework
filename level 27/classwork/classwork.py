ada8b87eb505bf23db3ff01bb806ae626198c1ac
gikosin8 <giorgilekiashvili860@gmail.com>
2024-08-20 09:20:39 -0700

    commit

---
+++ level 27/classwork/classwork.py
@@ -0,0 +1,6 @@
+#              0        1     2    3      4           5
+products = ['carrot',  10,  20.1, True, False, 'CHAD STUDENTS']
+
+#print (products[5])
+products[1] = '1000'
+print(products)
